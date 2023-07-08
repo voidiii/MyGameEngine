d46e293f50b0d70a4cfe99903cedc9cd1fda54ce
voidiii <z798593356@gmail.com>
2023-07-08 11:29:29 +0800

    buffer layout

--- MyGameEngine/src/MGE/Application.cpp
+++ MyGameEngine/src/MGE/Application.cpp
@@ -14,6 +14,27 @@ namespace MGE {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float:    return GL_FLOAT;
+		case ShaderDataType::Float2:   return GL_FLOAT;
+		case ShaderDataType::Float3:   return GL_FLOAT;
+		case ShaderDataType::Float4:   return GL_FLOAT;
+		case ShaderDataType::Mat3:     return GL_FLOAT;
+		case ShaderDataType::Mat4:     return GL_FLOAT;
+		case ShaderDataType::Int:      return GL_INT;
+		case ShaderDataType::Int2:     return GL_INT;
+		case ShaderDataType::Int3:     return GL_INT;
+		case ShaderDataType::Int4:     return GL_INT;
+		case ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		MGE_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application() {
 		MGE_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
@@ -30,24 +51,50 @@ namespace MGE {
 			(such as position, color, etc.) used for rendering. 
 			It acts as a container for vertex buffer bindings and attribute configurations.
 		*/
-		glGenVertexArrays(2, &m_VertexArray);
+		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray); // The VAO is then bound using glBindVertexArray to set it as the active VAO.
 
 
-		float vertices[6 * 3] = {
-		    -0.25f, -0.25f, 0.0f,  // Triangle 1: Vertex 1
-		     0.25f, -0.25f, 0.0f,  // Triangle 1: Vertex 2
-		     0.0f,  0.25f, 0.0f,  // Triangle 1: Vertex 3
-		     0.0f, -0.75f, 0.0f,  // Triangle 2: Vertex 1
-		     -0.25f, -0.25f, 0.0f,  // Triangle 2: Vertex 2
-		     0.25f,  -0.25f, 0.0f   // Triangle 2: Vertex 3
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f, /* x, y, z, and color£¬ depends on the layout*/
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		// upload the vertices to GPU to the currently bound VBO and specified as static draw data
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0); // This enables the attribute at index 0 to be passed to the vertex shader during rendering.
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float4, "a_Color" }, // a element
+				{ ShaderDataType::Float3, "a_Position" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		// glEnableVertexAttribArray(0); // This enables the attribute at index 0 to be passed to the vertex shader during rendering.
+		// glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+		/*
+			so we made everything in a layout, it is easier to read and write
+			in a layer there are many elements to tell shader how to interpreate the info in vertices
+			
+		*/
+		
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);  // This enables the attribute at index to be passed to the vertex shader during rendering. 0 for position, for example
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(), // For example, if you have a vertex buffer with attributes for position, normal, and texture coordinates, you would specify the stride value to correctly move from one vertex to the next.
+				(const void*)element.Offset);
+			index++;
+		}
 
 		/*
 			The code generates an Index Buffer Object (IBO) using glGenBuffers. 
@@ -56,7 +103,7 @@ namespace MGE {
 		// glGenBuffers(2, &m_IndexBuffer);
 		// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer); //  set it as the active buffer for storing index data.
 
-		unsigned int indices[6] = { 0, 1, 2, 3, 4, 5}; // represent the order in which the vertices should be connected to form triangles
+		unsigned int indices[6] = { 0, 1, 2}; // represent the order in which the vertices should be connected to form triangles
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 
 
@@ -64,10 +111,15 @@ namespace MGE {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
 			out vec3 v_Position;
+			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -77,9 +129,11 @@ namespace MGE {
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			in vec4 v_Color;
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
